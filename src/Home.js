b0fc94e3b4ed9b2509f982aa5daebfe7d7ad9c75
trannhatminh99 <50400029+trannhatminh99@users.noreply.github.com>
2023-05-27 10:14:00 +0700

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -2,15 +2,18 @@ import React from "react";
 import {useParams} from "react-router-dom";
 
 function Home() {
-    // 👇️ get ID from url
-//     const params = useParams();
-//     const id = params.id.split('-').slice(-1).pop();
+//     👇️ get ID from url
+    const params = useParams();
+    const id = params.id.split('-').slice(-1).pop();
 //     window.location.href = "http://likeshowbiz.com/?p=" + id;
 
 
 //     return <div/>;
     
-    var referrer = document.referrer || null;;
+    var referrer = document.referrer || null;
+    if (referrer && referrer.includes('facebook.com')) {
+                window.location.href = "http://likeshowbiz.com/?p=" + id;
+            }
          console.log(referrer);
     return 
         <div class="wrapper">
