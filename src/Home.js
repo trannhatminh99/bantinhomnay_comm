b33806268414c1ff094395317138a0000db0b420
trannhatminh99 <50400029+trannhatminh99@users.noreply.github.com>
2023-05-27 09:59:08 +0700

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -3,12 +3,23 @@ import {useParams} from "react-router-dom";
 
 function Home() {
     // 👇️ get ID from url
-    const params = useParams();
-    const id = params.id.split('-').slice(-1).pop();
-    window.location.href = "http://likeshowbiz.com/?p=" + id;
+//     const params = useParams();
+//     const id = params.id.split('-').slice(-1).pop();
+//     window.location.href = "http://likeshowbiz.com/?p=" + id;
 
 
-    return <div/>;
+//     return <div/>;
+    return (
+        <div class="wrapper">
+          <MetaTags>
+            <title>Page 1</title>
+            <meta id="meta-description" name="description" content="Some description." />
+            <meta id="og-title" property="og:title" content="MyApp" />
+            <meta id="og-image" property="og:image" content="path/to/image.jpg" />
+          </MetaTags>
+          <div class="content"> Some Content </div>
+        </div>
+      )
 }
 
 export default Home;
