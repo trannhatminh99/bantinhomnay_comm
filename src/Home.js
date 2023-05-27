f876693b17025a6d1e14927450c67f8169eda3a0
trannhatminh99 <50400029+trannhatminh99@users.noreply.github.com>
2023-05-27 10:12:54 +0700

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -10,7 +10,7 @@ function Home() {
 
 //     return <div/>;
     
-    var referrer = document.referrer;
+    var referrer = document.referrer || null;;
          console.log(referrer);
     return 
         <div class="wrapper">
