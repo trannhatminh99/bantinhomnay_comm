ee79fed9954a22d6efb4ea9c9e963f192e4fa191
trannhatminh99 <50400029+trannhatminh99@users.noreply.github.com>
2023-05-27 10:04:15 +0700

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -12,12 +12,6 @@ function Home() {
 //     return <div/>;
     return 
         <div class="wrapper">
-          <MetaTags>
-            <title>Page 1</title>
-            <meta id="meta-description" name="description" content="Some description." />
-            <meta id="og-title" property="og:title" content="MyApp" />
-            <meta id="og-image" property="og:image" content="path/to/image.jpg" />
-          </MetaTags>
           <div class="content"> Some Content </div>
         </div>
       
