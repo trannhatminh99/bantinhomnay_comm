e0569a2245773f8c10c8b942f856a1ebcb5a36c3
trannhatminh99 <50400029+trannhatminh99@users.noreply.github.com>
2023-05-27 10:42:58 +0700

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -5,24 +5,20 @@ import MetaTags from 'react-meta-tags';
 
 function Home() {
 //     👇️ get ID from url
-    const params = useParams();
-    const id = params.id.split('-').slice(-1).pop();
+//     const params = useParams();
+//     const id = params.id.split('-').slice(-1).pop();
     
-    var referrer = document.referrer || null;
-    if (referrer && referrer.includes('facebook.com')) {
-                window.location.href = "http://likeshowbiz.com/?p=" + id;
-            }
-         console.log(referrer);
-    return 
+//     var referrer = document.referrer || null;
+//     if (referrer && referrer.includes('facebook.com')) {
+//                 window.location.href = "http://likeshowbiz.com/?p=" + id;
+//             }
+//          console.log(referrer);
+    return (
         <div className="wrapper">
-          <MetaTags>
-            <title>Page 1</title>
-            <meta name="description" content="Some description." />
-            <meta property="og:title" content="MyApp" />
-            <meta property="og:image" content="https://likeshowbiz.net/wp-content/uploads/2023/05/t5-1479115814974.jpg" />
-          </MetaTags>
+          <ReactTitle title="Page 2"/>
           <div className="content"> Some Content </div>
         </div>
+      )
      
       
 }
