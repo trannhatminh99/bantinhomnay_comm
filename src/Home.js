dc2eec5d2deb212dfc07569c833ca34d15556add
trannhatminh99 <50400029+trannhatminh99@users.noreply.github.com>
2023-05-27 10:39:11 +0700

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -14,9 +14,16 @@ function Home() {
             }
          console.log(referrer);
     return 
-        <div class="wrapper">
-          <div class="content"> Some Content </div>
+        <div className="wrapper">
+          <MetaTags>
+            <title>Page 1</title>
+            <meta name="description" content="Some description." />
+            <meta property="og:title" content="MyApp" />
+            <meta property="og:image" content="path/to/image.jpg" />
+          </MetaTags>
+          <div className="content"> Some Content </div>
         </div>
+     
       
 }
 
